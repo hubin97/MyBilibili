39e955fadea5fb44c7040ec15dfbef2003d0bd7b
Hubin_Huang <970216474@qq.com>
2016-09-15 11:26:58 +0800

    Initial Commit

---
+++ MyBilibili/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyBilibili
+//
+//  Created by Hubin_Huang on 16/9/15.
+//  Copyright © 2016年 Hubin_Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
