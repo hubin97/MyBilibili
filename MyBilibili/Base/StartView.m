86695134ab32504b2bea9fe6a21290cc8319c715
Hubin_Huang <970216474@qq.com>
2016-10-20 22:41:05 +0800

    完善启动页面动画

--- MyBilibili/Base/StartView.m
+++ MyBilibili/Base/StartView.m
@@ -10,6 +10,10 @@
 #import "Masonry.h"
 
 @implementation StartView
+{
+    NSTimer *_myTimer;
+    NSInteger _skipTime;//跳过的时间
+}
 
 + (void)lanuchStart
 {
@@ -18,43 +22,74 @@
 
 - (id)init
 {
-    if (self = [super init])
+    self = [super initWithFrame:[UIScreen mainScreen].bounds];
+    
+    if (self)
     {
-        //默认启动图方式
-        __block StartView *weakStart = self;
-        UIImage *bg  = [UIImage imageNamed:@"bilibili_splash_iphone_bg"];
-        UIImage *img = [UIImage imageNamed:@"bilibili_splash_default"];
-
-        UIImageView *startBgView  = [[UIImageView alloc]init];
-        UIImageView *startImgView = [[UIImageView alloc]init];
-        startBgView.image = bg;
-
-        [self addSubview:startBgView];
-        [self addSubview:startImgView];
-        [self bringSubviewToFront:startImgView];
-        
-        [startBgView  mas_makeConstraints:^(MASConstraintMaker *make) {
-            
-            make.edges.equalTo(weakStart);
-        }];
-        
-        [startImgView mas_makeConstraints:^(MASConstraintMaker *make) {
-
-            make.size.mas_equalTo(CGSizeMake(img.size.width, img.size.height));
-            make.center.equalTo(startBgView);
-        }];
-
-        [UIView animateWithDuration:1.5 animations:^{
-            
-        } completion:^(BOOL finished) {
-            
-        }];
-
-
+        _skipTime = 3;
+        [self loadDefaultSubviews];
         
         //与服务器交互后加载
-        return self;
+        [self loadAction];
     }
     return self;
+}
+
+- (void)loadAction
+{
+    _myTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerAction) userInfo:nil repeats:YES];
+}
+
+- (void)timerAction
+{
+    _skipTime --;
+    if (!_skipTime)
+    {
+        [_myTimer invalidate];
+        _myTimer = nil;
+        [self removeFromSuperview];
+    }
+}
+
+- (void)loadDefaultSubviews
+{
+    //默认启动图方式
+    __block StartView *weakStart = self;
+    UIImage *bg  = [UIImage imageNamed:@"bilibili_splash_iphone_bg"];
+    UIImage *img = [UIImage imageNamed:@"bilibili_splash_default"];
+    
+    UIImageView *startBgView  = [[UIImageView alloc]init];
+    UIImageView *startImgView = [[UIImageView alloc]init];
+    startBgView.image  = bg;
+    startImgView.image = img;
+    
+    startBgView.contentMode  = UIViewContentModeScaleAspectFit;
+    startImgView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    [self addSubview:startBgView];
+    [self addSubview:startImgView];
+    [self bringSubviewToFront:startImgView];
+    
+    [startBgView  mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.edges.equalTo(weakStart);
+    }];
+    
+    [startImgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.size.mas_equalTo(CGSizeMake(100, 100));
+        make.center.equalTo(startBgView);
+    }];
+    
+    
+    [UIView animateWithDuration:0.8 animations:^{
+        
+        [startImgView mas_remakeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(weakStart.mas_width);
+            make.height.mas_equalTo(weakStart.mas_height).multipliedBy(0.8);
+            make.top.left.right.mas_equalTo(weakStart);
+        }];
+        [startImgView layoutIfNeeded];
+    }];
 }
 @end
