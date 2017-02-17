50a0e8200d19a71c02da3420d8928cefdd15f644
Hubin_Huang <970216474@qq.com>
2017-02-17 22:13:06 +0800

    添加Home列表视图

--- MyBilibili/Modules/Home/Controller/HomeViewController.m
+++ MyBilibili/Modules/Home/Controller/HomeViewController.m
@@ -8,7 +8,7 @@
 
 #import "HomeViewController.h"
 
-@interface HomeViewController ()
+@interface HomeViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
 
 @end
 
@@ -22,6 +22,8 @@
     
     //DrawViewBorderRadius(self.view, 0, 2, [UIColor redColor]);
     [self initNavi];
+    
+    [self initCollection];
 }
 
 #pragma mark - Setup
@@ -34,7 +36,7 @@
 
     [naviBar mas_makeConstraints:^(MASConstraintMaker *make) {
         make.width.mas_equalTo(kScreenW);
-        make.height.mas_equalTo(@52);
+        make.height.mas_equalTo(@(52 *k5SWScale));
     }];
     
     //测试
@@ -75,6 +77,64 @@
     }
 }
 
+- (void)initCollection
+{
+//    UIView *bgView = [[UIView alloc]init];
+//    [self.view addSubview:bgView];
+//    [bgView setBackgroundColor:cherryPowder];
+//    DrawViewBorderRadius(bgView, 1, 1, [UIColor blueColor]);
+//    
+//    [bgView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.edges.insets = UIEdgeInsetsMake(52 *k5SWScale, 0, 49, 0);
+//    }];
+    
+    
+    int col = 2; //列
+    CGFloat padding = 10 * k5SWScale;
+    CGFloat itemW = (kScreenW - (col + 1)*padding)/col;
+    CGFloat itemH = itemW *3 / 4;
+    
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
+    //4 : 3
+    layout.itemSize = CGSizeMake(itemW, itemH);
+    
+    layout.minimumLineSpacing = padding;
+    layout.minimumInteritemSpacing = padding;
+    layout.sectionInset = UIEdgeInsetsMake(0, padding, 0, padding);
+    
+    [layout setHeaderReferenceSize:CGSizeMake(kScreenW, 40)]; //设置headview 的大小
+
+    
+    UICollectionView *collectionView = [[UICollectionView alloc]initWithFrame:CGRectZero collectionViewLayout:layout];
+    collectionView.backgroundColor = [UIColor whiteColor];
+    collectionView.showsHorizontalScrollIndicator = NO;
+    collectionView.showsVerticalScrollIndicator = NO;
+    
+    [self.view addSubview:collectionView];
+
+    collectionView.dataSource = self;
+    collectionView.delegate = self;
+    
+    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"HomeCell"];
+    
+    //DrawViewBorderRadius(collectionView, 1, 1, [UIColor brownColor]);
+    
+    [collectionView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.insets = UIEdgeInsetsMake(52 *k5SWScale, 0, 49, 0);
+    }];
+    
+//    collectionView.bouncesZoom = NO;
+    collectionView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    collectionView.contentOffset = CGPointMake(0, 0);
+    
+    
+
+    //[collectionView bringSubviewToFront:self.view];
+
+    //collectionView.foo
+}
+
+
 
 #pragma mark - Action
 - (void)changeTitleBtn:(UIButton *)sender
@@ -87,6 +147,30 @@
         //NSLog(@"titleFlag:%@",titleFlag);
         [titleFlag setBackgroundColor:((i == (sender.tag + 10))?[UIColor whiteColor] : [UIColor clearColor])];
     }
+}
+
+
+#pragma mark - coll
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 5;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 6;
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdent = @"HomeCell";
+
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdent forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor whiteColor];
+    
+    DrawViewBorderRadius(cell, 1, 1, [UIColor redColor]);
+    return cell;
 }
 
 @end
